75fa69947bd96dad1b28a1c6fab61b765151d49f
PragalbhRai <pragalbhrai@gmail.com>
2026-03-09 09:05:33 +0530

    UC1: Application entry point and welcome message

---
+++ src/BookMyStay.java
@@ -0,0 +1,20 @@
+/**
+ * Book My Stay Application
+ *
+ * Entry point of the Hotel Booking Management System.
+ *
+ * @author Student
+ * @version 1.0
+ */
+
+public class BookMyStay {
+
+    public static void main(String[] args) {
+
+        System.out.println("===================================");
+        System.out.println(" Welcome to Book My Stay App ");
+        System.out.println(" Hotel Booking System v1.0 ");
+        System.out.println("===================================");
+
+    }
+}
