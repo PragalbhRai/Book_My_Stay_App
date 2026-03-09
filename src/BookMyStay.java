4af66e5b5dc7c78c35757c7a943f979ae58773ec
PragalbhRai <pragalbhrai@gmail.com>
2026-03-09 09:09:59 +0530

    UC2: Room modeling using abstraction and inheritance

--- src/BookMyStay.java
+++ src/BookMyStay.java
@@ -1,20 +1,81 @@
 /**
  * Book My Stay Application
  *
- * Entry point of the Hotel Booking Management System.
+ * Use Case 2: Basic Room Types & Static Availability
+ * Demonstrates abstraction, inheritance, and polymorphism
+ * in a simple hotel booking domain model.
  *
  * @author Student
- * @version 1.0
+ * @version 2.1
  */
+
+abstract class Room {
+
+    protected String roomType;
+    protected int beds;
+    protected double price;
+
+    public Room(String roomType, int beds, double price) {
+        this.roomType = roomType;
+        this.beds = beds;
+        this.price = price;
+    }
+
+    public void displayDetails() {
+        System.out.println("Room Type: " + roomType);
+        System.out.println("Beds: " + beds);
+        System.out.println("Price per night: $" + price);
+    }
+}
+
+class SingleRoom extends Room {
+    public SingleRoom() {
+        super("Single Room", 1, 100.0);
+    }
+}
+
+class DoubleRoom extends Room {
+    public DoubleRoom() {
+        super("Double Room", 2, 180.0);
+    }
+}
+
+class SuiteRoom extends Room {
+    public SuiteRoom() {
+        super("Suite Room", 3, 350.0);
+    }
+}
 
 public class BookMyStay {
 
     public static void main(String[] args) {
 
         System.out.println("===================================");
-        System.out.println(" Welcome to Book My Stay App ");
-        System.out.println(" Hotel Booking System v1.0 ");
+        System.out.println(" Book My Stay - Hotel Booking App ");
+        System.out.println(" Version 2.1 ");
         System.out.println("===================================");
 
+        // Create room objects using polymorphism
+        Room single = new SingleRoom();
+        Room doubleRoom = new DoubleRoom();
+        Room suite = new SuiteRoom();
+
+        // Static availability variables
+        int singleAvailable = 5;
+        int doubleAvailable = 3;
+        int suiteAvailable = 2;
+
+        System.out.println("\n--- Room Details & Availability ---");
+
+        single.displayDetails();
+        System.out.println("Available: " + singleAvailable);
+        System.out.println();
+
+        doubleRoom.displayDetails();
+        System.out.println("Available: " + doubleAvailable);
+        System.out.println();
+
+        suite.displayDetails();
+        System.out.println("Available: " + suiteAvailable);
     }
 }
